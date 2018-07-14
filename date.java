5cdd881ae3464d43b0c581d3964807e65603753a
parthatom <parth.shxh@gmail.com>
2018-07-14 22:16:27 +0530

    add a date feature for the bill

---
+++ date.java
@@ -0,0 +1,9 @@
+import java.util.*;  
+public class date 
+{
+   public void date1() 
+   {      
+       Date date = new Date();       
+       System.out.println("\t"+"\t"+"\t"+"\t"+date.toString());
+   }
+}
